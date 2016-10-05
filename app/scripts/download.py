b59ae473f332b1329e44ed329d368778fe2be5ac
Ricardo Lobo <ricardolobo@audienciazero.org>
2016-10-05 13:01:37 +0100

    adds option to save to a specified folder, and deals with some requests exceptions

--- app/scripts/download.py
+++ app/scripts/download.py
@@ -5,6 +5,7 @@ import shutil
 import argparse
 
 from urllib.parse import urlparse
+from requests import exceptions
 
 IMAGE_DIR = '/app/images'
 
@@ -15,38 +16,54 @@ def get_filename(url):
     parsed = urlparse(url)
     return parsed.path.split('/')[-1:][0]
 
-def download_image(url, folder=IMAGE_DIR, filename=None):
+def download_image(url, directory, filename=None):
     """
     Downloads an image from an url
     """
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
     if not filename:
         filename = get_filename(url).strip()
 
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        with open(os.path.join(IMAGE_DIR, filename), 'wb') as f:
-            response.raw.decode_content = True
-            shutil.copyfileobj(response.raw, f)
+    try:
+        response = requests.get(url, stream=True, timeout=5)
+        if response.status_code == 200:
+            with open(os.path.join(directory, filename), 'wb') as f:
+                response.raw.decode_content = True
+                shutil.copyfileobj(response.raw, f)
+    except exceptions.ConnectionError:
+        sys.stdout.write("Connection problem skipping: %s   \r" % (url) )
+    except exceptions.Timeout:
+        sys.stdout.write("Connection timeout skipping: %s   \r" % (url) )
+    except exceptions.TooManyRedirects:
+        sys.stdout.write("Too many redirects skipping: %s   \r" % (url) )
 
-def download_from_file(f):
+def download_from_file(f, directory):
     """
     Reads urls from a file and downloads
     """
-    with open(f, 'r') as file:
+    with open(f, 'r', encoding='utf8') as file:
         for url in file:
-            download_image(url)
+            download_image(url, directory)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hacker Buzz Utils')
     parser.add_argument('-f', '--file', help='File with list of images urls to download.')
+    parser.add_argument('-d', '--directory', default=IMAGE_DIR, help='Where to store the images, a subdirectory of IMAGE_DIR, created if does not exist.')
     args = parser.parse_args()
 
+    if args.directory != IMAGE_DIR:
+        directory = os.path.join(IMAGE_DIR, args.directory)
+    else:
+        directory = IMAGE_DIR
+
     if args.file:
-        download_from_file(args.file)
+        download_from_file(args.file, directory)
     else:
         try:
             url = sys.argv[1]
-            download_image(url)
+            download_image(url, directory)
         except IndexError:
             print('Please provide the image url or a file with a list of urls.')
             sys.exit()
