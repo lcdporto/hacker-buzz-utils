aa4246e17dadd589c71f4360be5a569ed2356167
Ricardo Lobo <ricardolobo@audienciazero.org>
2016-10-05 11:39:51 +0100

    use an input file with urls to download images, adds argparse

--- app/scripts/download.py
+++ app/scripts/download.py
@@ -2,13 +2,15 @@ import os
 import sys
 import requests
 import shutil
+import argparse
+
 from urllib.parse import urlparse
 
 IMAGE_DIR = '/app/images'
 
 def get_filename(url):
     """
-    Returns the last path segment to be used as filename
+    Returns last path segment to be used as filename
     """
     parsed = urlparse(url)
     return parsed.path.split('/')[-1:][0]
@@ -18,21 +20,33 @@ def download_image(url, folder=IMAGE_DIR, filename=None):
     Downloads an image from an url
     """
     if not filename:
-        filename = get_filename(url)
-        
+        filename = get_filename(url).strip()
+
     response = requests.get(url, stream=True)
     if response.status_code == 200:
         with open(os.path.join(IMAGE_DIR, filename), 'wb') as f:
             response.raw.decode_content = True
-            shutil.copyfileobj(response.raw, f)  
+            shutil.copyfileobj(response.raw, f)
+
+def download_from_file(f):
+    """
+    Reads urls from a file and downloads
+    """
+    with open(f, 'r') as file:
+        for url in file:
+            download_image(url)
 
 if __name__ == '__main__':
-    try:
-        url = sys.argv[1]
-        download_image(url)
-    except IndexError:
-        print('Provide the image url...')
-        exit(1)
+    parser = argparse.ArgumentParser(description='Hacker Buzz Utils')
+    parser.add_argument('-f', '--file', help='File with list of images urls to download.')
+    args = parser.parse_args()
 
-    
-
+    if args.file:
+        download_from_file(args.file)
+    else:
+        try:
+            url = sys.argv[1]
+            download_image(url)
+        except IndexError:
+            print('Please provide the image url or a file with a list of urls.')
+            sys.exit()
